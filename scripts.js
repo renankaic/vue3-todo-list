0eb3a3ffdba447d0912242f12dabc226155b9215
Renan Kaic <renan-kaic@outlook.com>
2021-10-05 05:40:54 -0300

    Fix - Using "updated" lifecycle to save all changes in localStorage when changing a todo

--- scripts.js
+++ scripts.js
@@ -16,8 +16,12 @@ const todoList = {
             }
         }
     },
+    //Vue lifecycles hooks
     created() {
       this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos
+    },
+    updated() {
+        localStorage.setItem("todos", JSON.stringify(this.todos))
     }
 }
 
