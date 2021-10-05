a83b31b30b8dad2158ef6148b471ae2aa398ec0a
Renan Kaic <renan-kaic@outlook.com>
2021-10-05 05:45:04 -0300

    Fix - Improving localStorage update without using the "updated" lifecycle hook, which could cause poor performance in bigger applications

--- scripts.js
+++ scripts.js
@@ -14,14 +14,14 @@ const todoList = {
             } else {
                 alert("Texto da tarefa é obrigatório")
             }
+        },
+        storeTodos() {
+            localStorage.setItem("todos", JSON.stringify(this.todos))
         }
     },
     //Vue lifecycles hooks
     created() {
       this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos
-    },
-    updated() {
-        localStorage.setItem("todos", JSON.stringify(this.todos))
     }
 }
 
